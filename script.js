5b16cacaaf87c524f41d52894db2f7ae38482e30
hashim-ali-19 <ha4931221@gmail.com>
2026-09-18 15:55:42 +0500

    Task 14 — Click

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 let label = "Read";
 console.log(label);
-document.querySelector("#status");
+document.querySelector("#mark").addEventListener("click", function () {
+    document.querySelector("#status").textContent = label;
+});
